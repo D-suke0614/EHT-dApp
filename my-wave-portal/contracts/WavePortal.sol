91b1255e9fe0b0ec13a81f2621af4d8f898a0a7a
Daisuke <d.suke.kida@gmail.com>
2023-01-09 16:24:36 +0900

    create contract

---
+++ my-wave-portal/contracts/WavePortal.sol
@@ -0,0 +1,12 @@
+// WavePortal.sol
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Here is my first smart contract!");
+    }
+}
